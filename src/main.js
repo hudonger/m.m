790b14d890a2ef2724f2e864e6352305be50ea4b
donger <824331661@qq.com>
2018-11-25 13:51:37 +0800

    main.js 缩进

--- src/main.js
+++ src/main.js
@@ -1,35 +1,31 @@
 import 'babel-polyfill'
-if (process.env.NODE_ENV !== 'production') require('./mock')
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import store from './store'
-import NProgress from 'nprogress'
+import Vue          from 'vue'
+import App          from './App'
+import router       from './router'
+import store        from './store'
+import NProgress    from 'nprogress'
+import toast        from './components/toast'
+import VueLazyload  from 'vue-lazyload'
+import './libs/rem'
 import 'nprogress/nprogress.css'
 import './assets/styles/reset.css'
 import './assets/styles/base.scss'
 import './assets/fonts/iconfont.css'
 import './assets/styles/animate.css'
 
+if (process.env.NODE_ENV !== 'production') require('./mock')
+
+Vue.use(toast)
+
 NProgress.configure({ showSpinner: false })
 Vue.prototype.$progress = NProgress
 
-// 初始化 font-size
-import './libs/rem'
-
-// 全局提示插件
-import toast from './components/toast'
-Vue.use(toast)
-
-// 图片懒加载
-import VueLazyload from 'vue-lazyload'
 Vue.use(VueLazyload, {
   loading: require('./assets/images/load.svg')
 })
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
